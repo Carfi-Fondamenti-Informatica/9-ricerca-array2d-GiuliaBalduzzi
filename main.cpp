92b5cfef7d36051842ed8ea63b33f8b3f3e9f2c8
GiuliaBalduzzi <91681343+GiuliaBalduzzi@users.noreply.github.com>
2021-12-11 15:18:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 int main() {
-    int pos;
+    int posizione;
     char a[10][20], f[1][20];
     for (int i=0; i<10; i++){
         cin >> a[i];
     }
     cin >> f[0];
 
-    if (ricerca (a,f, pos)){
+    if (cerca (a,f, posizione)) {
+        cout << posizione << endl;
+    }else{
         cout << "non presente" << endl;
     }
     return 0;
 }
+
+
+
 
 
