92d70acd1e69702f427801f83898a1a74833b9ec
GiuliaBalduzzi <91681343+GiuliaBalduzzi@users.noreply.github.com>
2021-12-11 15:21:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,7 +5,7 @@ bool ricerca (char lista [10][20], char nome [1][20], int &pos){
 
     for (int i=0; i<10; i++){
         if (strcmp (lista [i], nome [0]) ==0){
-            pos = 1;
+            posizione = 1;
             return true;
         }
     }
