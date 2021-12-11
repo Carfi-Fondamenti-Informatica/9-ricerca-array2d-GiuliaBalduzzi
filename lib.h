41d3333439eeb6ee5128fa2c2d2fe1e364d0dfa0
GiuliaBalduzzi <91681343+GiuliaBalduzzi@users.noreply.github.com>
2021-12-11 14:56:18 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by ICM on 11/12/2021.
+//
 
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+
+bool ricerca (char lista [10][20], char nome [1][20], int &pos);
+
+#endif //ESERCITAZIONE_9_LIB_H
